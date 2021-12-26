0f22f8be3b9b8621bb99df78b7f71d83e27eb03a
Han Ke <han.von.ke@gmail.com>
2021-12-26 17:41:53 -0500

    no more region api, hardcode LA, update to match new api

--- script/get_zillow_listings.py
+++ script/get_zillow_listings.py
@@ -5,7 +5,7 @@ from xml.etree import ElementTree
 def main(argv):
   state, city = parse_args(argv)
 
-  region_id = get_zillow_region_id(state, city)
+  region_id = '12447' # get_zillow_region_id(state, city)
   print('zillow region id: ' + region_id)
 
   listings = get_zillow_listings_for_region(region_id)
@@ -28,32 +28,33 @@ def get_zillow_region_id(state, city):
   return ElementTree.fromstring(response.content).find('response').find('region').find('id').text
 
 def get_zillow_listings_for_region(region_id):
-  data = fetch_zillow_listings(region_id)
-  total_pages = data['searchList']['totalPages']
-  listings = data['searchResults']['listResults']
+  page = 1
+  data = fetch_zillow_listings(region_id, page)
+  total_result_count = data['cat1']['searchList']['totalResultCount']
+  listings = data['cat1']['searchResults']['listResults']
 
-  for page in range(2, total_pages + 1):
+  while total_result_count > 0:
+    page += 1
     data = fetch_zillow_listings(region_id, page)
-    listings += data['searchResults']['listResults']
+    listings += data['cat1']['searchResults']['listResults']
+    total_result_count = data['cat1']['searchList']['totalResultCount']
 
   return list({v['zpid']:v for v in listings}.values())
 
 def fetch_zillow_listings(region_id, page = 1):
+  url = f"https://www.zillow.com/search/GetSearchPageState.htm?{build_search_query(region_id, page)}"
+  print(url)
   response = requests.get(
-    'https://www.zillow.com/search/GetSearchPageState.htm',
-    params={
-      'searchQueryState': build_search_query(region_id, page),
-      'includeMap': 'false',
-      'includeList': 'true'
-    },
+    url,
     headers={
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36',
-      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+      'user-agent': 'Mozilla/5.0 (compatible; MSIE 5.0; Windows NT 5.2; Trident/5.1)',
+      'accept': '*/*',
       'accept-encoding': 'gzip, deflate, br',
       'accept-language': 'en-US,en;q=0.9',
-      'sec-fetch-mode': 'navigate',
-      'sec-fetch-site': 'none',
-      'upgrade-insecure-requests': '1'
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-dest': 'empty',
+      'sec-gpc': '1',
+      'sec-fetch-site': 'none'
     }
   )
 
@@ -65,12 +66,45 @@ def fetch_zillow_listings(region_id, page = 1):
   return response.json()
 
 def build_search_query(region_id, page):
+  min = 500000
+  max = 2000000
+
   # need to url encode a json string so it can be used as a query param
   # lat/lng is required and is hard coded to cover continental usa to allow any american city
-  # price range hard coded to 50k-200k
-  return urllib.parse.quote(
-    "{\"pagination\":{\"currentPage\":%i},\"mapBounds\":{\"west\":-124.848974,\"east\":-66.885444,\"south\":24.396308,\"north\":49.384358},\"regionSelection\":[{\"regionId\":%s,\"regionType\":6}],\"isMapVisible\":false,\"filterState\":{\"price\":{\"min\":50000,\"max\":200000}},\"isListVisible\":true}"%(page, region_id)
+  # price range hard coded to 500k-2m
+  return urllib.parse.urlencode(
+    {
+      'searchQueryState': {
+          "pagination": {"currentPage": page},
+          "mapBounds": {
+              "west": -124.848974,
+              "east": -66.885444,
+              "south": 24.396308,
+              "north": 49.384358
+          },
+          "regionSelection": [{"regionId": region_id, "regionType": 6}],
+          "isMapVisible": False,
+          "filterState": {
+              "beds": {"min": 2}, "baths":{"min": 2},
+              "isMultiFamily": {"value":False}, "isApartmentOrCondo": {"value":False}, "isApartment": {"value":False}, "isCondo": {"value":False},
+              "isLotLand": {"value": False}, "isManufactured": {"value":False},
+              "price": {"min": min, "max": max}
+              # "doz": {"value": "6m"}, "isForSaleByAgent": {"value": False},
+              # "isForSaleByOwner": {"value": False}, "isNewConstruction": {"value": False},
+              # "isForSaleForeclosure": {"value": False}, "isComingSoon": {"value": False},
+              # "isAuction": {"value": False}, "isPreMarketForeclosure": {"value": False},
+              # "isPreMarketPreForeclosure": {"value": False},
+              # "isRecentlySold": {"value": True}, "isAllHomes": {"value": True},
+              # "hasPool": {"value": True}, "hasAirConditioning": {"value": True},
+              # "isApartmentOrCondo": {"value": False},
+          },
+          "isListVisible": True
+      },
+      'wants': {"cat1": ["listResults"], "cat2": ["total"]},
+      'requestId': page
+    }
   )
+  # "{\"pagination\":{\"currentPage\":%i},\"mapBounds\":{\"west\":-124.848974,\"east\":-66.885444,\"south\":24.396308,\"north\":49.384358},\"regionSelection\":[{\"regionId\":%s,\"regionType\":6}],\"isMapVisible\":false,\"filterState\":{\"price\":{\"min\":50000,\"max\":200000}},\"isListVisible\":true}"%(page, region_id)
 
 def write_listings_to_file(listings, state, city, region_id):
   # flatten the data so it can be stored as a csv
@@ -85,7 +119,8 @@ def write_listings_to_file(listings, state, city, region_id):
   return filename
 
 def transform_listing(listing):
-  additional_data = fetch_additional_data(listing)
+  # additional_data = fetch_additional_data(listing)
+  # print(additional_data)
   homeInfo = listing['hdpData']['homeInfo']
   return {
     'address': listing['address'],
@@ -94,7 +129,11 @@ def transform_listing(listing):
     'status': homeInfo.get('homeStatus', None),
     'other_status': listing['statusText'],
     'price': homeInfo.get('price', None),
-    'last_sold_price': additional_data.get('lastSoldPrice', None),
+    # 'last_sold_price': additional_data.get('lastSoldPrice', None),
+    # 'tax_assessed_value': additional_data.get('taxAssessedValue', None),
+    # 'tax_assessed_year': additional_data.get('taxAssessedYear', None),
+    # 'mortgage_rate': additional_data['mortgageRates'].get('thirtyYearFixedRate', None),
+    # 'propert_tax_rate': additional_data.get('propertyTaxRate', None),
     'zestimate': homeInfo.get('zestimate', None),
     'festimate': homeInfo.get('festimate', None),
     'rent_zestimate': homeInfo.get('rentZestimate', None),
@@ -102,11 +141,8 @@ def transform_listing(listing):
     'baths': listing['baths'],
     'area': listing['area'],
     'year': homeInfo.get('yearBuilt', None),
-    'reduction': homeInfo.get('priceReduction', None),
-    'tax_assessed_value': additional_data.get('taxAssessedValue', None),
-    'tax_assessed_year': additional_data.get('taxAssessedYear', None),
-    'mortgage_rate': additional_data.get('thirtyYearFixedRate', None),
-    'propert_tax_rate': additional_data.get('propertyTaxRate', None),
+    'price_reduction': homeInfo.get('priceReduction', None),
+    'price_increase': homeInfo.get('priceChange', None),
     'days_on_zillow': homeInfo.get('daysOnZillow', None),
     'extra_info': listing['variableData']['text'],
     'extra_info_type': listing['variableData']['type'],
@@ -161,7 +197,7 @@ def fetch_additional_data(listing):
     'lastSoldPrice': data['lastSoldPrice'],
     'taxAssessedValue': data['taxAssessedValue'],
     'taxAssessedYear': data['taxAssessedYear'],
-    'thirtyYearFixedRate': data['mortgageRates']['thirtyYearFixedRate'],
+    'mortgageRates': data.get('mortgageRates', { 'thirtyYearFixedRate': None }),
     'propertyTaxRate': data['propertyTaxRate'],
   }
 
